ceee2a1f22a0995cdebf5cab6679bbd4e332e2aa
Joyce Wang <joycewang@wireless-10-147-117-107.public.utexas.edu>
2021-09-22 09:48:45 -0500

    Added minor changes to scripts and lab notebook

--- scripts/00_make_directories.sh
+++ scripts/00_make_directories.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-#
-#SBATCH --account=mfplab
-#SBATCH --job-name=prepare_directories
-#SBATCH -c 1
-#SBATCH --time=5:00
-#SBATCH --mem-per-cpu=1gb
+#SBATCH -J prepare_directories
+#SBATCH -o prepare_directories.o%j
+#SBATCH -e prepare_directories.o%j
+#SBATCH -p normal
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -t 00:05:00
+#SBATCH -A Harpak-Lab-GWAS
+#SBATCH --mail-user=joyce.wang@utexas.edu
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
 
-mkdir data/ambiguous_indel_snps \
+mkdir data \
+      data/ambiguous_indel_snps \
       data/intersecting_filtered \
       data/kgp_filtered \
       data/kgp_merged \
@@ -14,20 +20,20 @@ mkdir data/ambiguous_indel_snps \
       data/ukb_filtered \
       data/ukb_merged \
       data/ukb_meta \
-      data/ukb_populations \ #will include separated pop groups
-      data/models \
+      data/ukb_populations #will include separated pop groups
+mkdir data/models \
       data/phenotypes \
       data/gwas_results \
-      data/prs \ #only scores from C+T approach
-      data/kgp_populations \
-      data/fst \ 
-      data/LDpred \ 
-      data/LDpred/prs \ #contains PRS for target individuals
-      data/LDpred/tmp-data \ #memory storage for LDpred code
-      data/LDpred/val_prs \ #contains PRS for validation set
-      data/prs_comparisons \ #contains agg data from C+T & LDpred
-      data/theory \  #LD & MAF patterns from target groups
-      data/theor_herit \  #LDSC results to calculate h2
-      data/theoretical \ #Destination of results from theor predictions
-      img \
+      data/prs #only scores from C+T approach
+mkdir data/kgp_populations \
+      data/fst \
+      data/LDpred \
+      data/LDpred/prs #contains PRS for target individuals
+mkdir data/LDpred/tmp-data #memory storage for LDpred code
+mkdir data/LDpred/val_prs #contains PRS for validation set
+mkdir data/prs_comparisons #contains agg data from C+T & LDpred
+mkdir data/theory #LD & MAF patterns from target groups
+mkdir data/theor_herit #LDSC results to calculate h2
+mkdir data/theoretical #Destination of results from theor predictions
+mkdir img
 
--- scripts/01_UKBB_genotypes_filtered.sh
+++ scripts/01_UKBB_genotypes_filtered.sh
@@ -1,40 +1,52 @@
 #!/bin/bash
-#
-#SBATCH --account=mfplab
-#SBATCH --job-name=make_filtered_genotypes_files
-#SBATCH -c 5
-#SBATCH --time=24:00:00
-#SBATCH --mem-per-cpu=16gb
+#SBATCH -J make_filtered_genotypes_files
+#SBATCH -o make_filtered_genotypes_files.o%j
+#SBATCH -e make_filtered_genotypes_files.o%j
+#SBATCH -p normal
+#SBATCH -N 1 #5
+#SBATCH -n 4 #16
+#SBATCH -t 1:00:00 #24:00:00
+#SBATCH -A Harpak-Lab-GWAS
+#SBATCH --mail-user=joyce.wang@utexas.edu
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
 
 set -e
 
-plink="/rigel/mfplab/users/jm4454/plink/plink"
-plink2="/rigel/mfplab/users/jm4454/plink/plink2"
+plink="/work2/06568/joyce_w/stampede2/software/plink/plink"
+plink2="/work2/06568/joyce_w/stampede2/software/plink/plink2"
 
-wb='/rigel/mfplab/projects/ukb_hakhamanesh/imputed/plink_neale'
-nonwb='/rigel/mfplab/projects/ukb_hakhamanesh/imputed/plink_neale_nonWB'
+wb='/corral-repl/utexas/Recombining-sex-chro/ukb/data/genotype_calls'
+#nonwb='/rigel/mfplab/projects/ukb_hakhamanesh/imputed/plink_neale_nonWB'
 
-module load anaconda
-source activate prs1
+#module load anaconda3
+#source activate prs1
+source /work2/06568/joyce_w/stampede2/software/anaconda3/etc/profile.d/conda.sh
+conda init bash
+conda activate prs1
 
-for i in $(seq 1 23);
+#for i in $(seq 1 23);
+for i in $(seq 22 22);
 do
   # Identify indels and ambiguous variants and write them to a file
-  python scripts2/01a_get_ambiguous_indel_snps.py \
-    ${wb}/chr${i}.bim \
+  python 01a_get_ambiguous_indel_snps.py \
+    # Having an erro running the following line: 
+    # FileNotFoundError: [Errno 2] No such file or directory: 
+    # '/corral-repl/utexas/Recombining-sex-chro/ukb/data/genotype_calls/ukb_snp_chr22_v2.bim'
+    ${wb}/ukb_snp_chr${i}_v2.bim \
     -o data/ambiguous_indel_snps/chr${i}.snps
 
   # Convert from Plink 1 to Plink 2 and compute the SNPs to be dropped
   $plink2 \
-    --bfile ${wb}/chr${i}  \
-    --keep ${wb}/ukb_white_indivs_neal.fam \
-    --remove /rigel/mfplab/projects/prs-portability/data/ukb_meta/excluded_samples.sam \
+    --bfile ${wb}/ukb_snp_chr${i}_v2  \
+    --keep ${wb}/ukb22418_chr${i}_b0_v2_s488244.fam \
+    --remove /work2/06568/joyce_w/stampede2/pgs_portability/data/ukb_meta/excluded_samples.sam \
     --exclude data/ambiguous_indel_snps/chr${i}.snps \
     --maf 0.01 \
     --geno 0.01 \
     --indep-pairwise 1000 kb 1 0.2 \
     --make-pgen \
-    --out data/ukb_filtered/wb_chr$i
+    --out data/ukb_filtered/wb_chr${i}
 
   # Write a new file using only filtered SNPs for 1000 Genomes (Plink 1 format)
   $plink2 \
@@ -47,18 +59,18 @@ do
   rm data/ukb_filtered/wb_chr${i}.pvar
   rm data/ukb_filtered/wb_chr${i}.psam 
 
-  $plink2 \
-    --bfile ${nonwb}/chr${i} \
-    --keep ${nonwb}/ukb_filter_indivs_nowhiteB_unrelated.txt \
-    --remove /rigel/mfplab/projects/prs-portability/data/ukb_meta/excluded_samples.sam \
-    --extract data/ukb_filtered/wb_chr${i}.prune.in \
-    --make-bed \
-    --out data/ukb_filtered/nonwb_chr${i}
+#  $plink2 \
+#    --bfile ${nonwb}/chr${i} \
+#    --keep ${nonwb}/ukb_filter_indivs_nowhiteB_unrelated.txt \
+#    --remove /rigel/mfplab/projects/prs-portability/data/ukb_meta/excluded_samples.sam \
+#    --extract data/ukb_filtered/wb_chr${i}.prune.in \
+#    --make-bed \
+#    --out data/ukb_filtered/nonwb_chr${i}
 
 
   # Append the output file path to a new file (for merging them all below)
   printf "data/ukb_filtered/wb_chr%s\n" $i >> data/ukb_merged/ukb_merged_list${i}.txt
-  printf "data/ukb_filtered/nonwb_chr%s\n" $i >> data/ukb_merged/ukb_merged_list${i}.txt
+#  printf "data/ukb_filtered/nonwb_chr%s\n" $i >> data/ukb_merged/ukb_merged_list${i}.txt
 
   $plink \
     --merge-list data/ukb_merged/ukb_merged_list${i}.txt \
@@ -66,7 +78,7 @@ do
     --out data/ukb_merged/chr${i}
 
   rm data/ukb_filtered/wb_chr${i}.b*
-  rm data/ukb_filtered/nonwb_chr${i}.b*
+#  rm data/ukb_filtered/nonwb_chr${i}.b*
   rm data/ukb_filtered/*.fam
   rm data/ukb_filtered/*.log
 
